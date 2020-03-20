451b92c7c420dd2799a47959aee8a69580cc9e18
Austen Turner <austenpturner@msn.com>
2020-03-19 17:39:11 -0700

    add functionality to update tables

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const inquirer = require('inquirer');
-const returnAll = require('./lib/render_objects');
+const returnAll = require('./lib/return_objects');
 const render = require('./lib/render_tables');
 const select = require('./model/select');
 const insertInto = require('./model/insert');
-const updateInfo = require('./model/update');
+const updateTable = require('./model/update');
 const deleteInfo = require('./model/delete');
 
 const startApp = () => {
@@ -166,13 +166,174 @@ const inquireUpdate = () => {
     });
 };
 
+const inquireDepartmentUpdate = () => {
+    getDepartments().then(res => {
+        if (res === 'No departments found.') {
+            return res;
+        } else {
+            const departments = res;
+            const departmentNames = getDepartmentNames(departments);
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    message: 'Select a department to update:',
+                    name: 'department',
+                    choices: departmentNames
+                },
+                {
+                    type: 'input',
+                    message: 'Enter new department name:',
+                    name: 'newDepartment'
+                }
+            ]).then(({department, newDepartment}) => {
+                updateTable.departmentTable(`name`, `'${newDepartment}'`,
+                `name = '${department}'`, () => {
+                    console.log(`\n${department} successfully updated to ${newDepartment}.\n`);
+                });
+                setTimeout( () => {
+                    inquireAgain();
+                }, 500);
+            });
+        };
+    });
+};
+
+const inquireRoleUpdate = () => {
+    getRoles().then(res => {
+        if (res === 'No roles found.') {
+            return res;
+        } else {
+            const roles = res;
+            const roleTitles = getRoleTitles(roles);
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    message: 'Select a role to update:',
+                    name: 'role',
+                    choices: roleTitles
+                },
+                {
+                    type: 'list',
+                    message: 'Select a role value to update:',
+                    name: 'column',
+                    choices: [
+                        'title',
+                        'salary'
+                    ]
+                },
+                {
+                    type: 'input',
+                    message: 'Enter updated value:',
+                    name: 'value'
+                }
+            ]).then(({role, column, value}) => {
+                if (column === 'salary') {
+                    value = parseInt(value);
+                } else {
+                    value = `'${value}'`;
+                }
+                updateTable.roleTable(column, value, `title = '${role}'`,  () => {
+                    console.log(`${role}'s ${column} was successfully updated to ${value}.`);
+                });
+                setTimeout( () => {
+                    inquireAgain();
+                }, 500);
+            });
+        };
+    });
+};
+
+const inquireEmployeeUpdate = () => {
+    getEmployees().then(res => {
+        if (res === 'No employees found.') {
+            return res;
+        } else {
+            const employees = res;
+            const employeeNames = getEmployeeNames(employees);
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    message: 'Select an employee to update:',
+                    name: 'employeeName',
+                    choices: employeeNames
+                },
+                {
+                    type: 'list',
+                    message: 'Select a employee value to update:',
+                    name: 'column',
+                    choices: [
+                        'First name',
+                        'Last name',
+                        'Role',
+                        'Manager Id'
+                    ]
+                }
+            ]).then(({employeeName, column}) => {
+                const employeeId = employeeName.split(' ').pop();
+                if (column === 'Role') {
+                    getRoles().then(res => {
+                        if (res === 'No roles found.') {
+                            return res;
+                        } else {
+                            const roles = res;
+                            const roleTitles = getRoleTitles(roles);
+                            inquirer.prompt([
+                                {
+                                    type: 'list',
+                                    message: 'Select a new role:',
+                                    name: 'role',
+                                    choices: roleTitles
+                                }
+                            ]).then(({role}) => {
+                                const roleId = getRoleId(roles, role);
+                                updateTable.employeeTable('role_id', roleId, `id = ${employeeId}`, () => {
+                                    console.log(`${column} was updated to ${role}.`);
+                                });
+                                setTimeout( () => {
+                                    inquireAgain();
+                                }, 500);
+                            });
+                        };
+                    });
+                } else {
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            message: `Enter new value for employee's ${column}:`,
+                            name: 'value'
+                        }
+                    ]).then(({value}) => {
+                        let colName;
+                        if (column === 'Manager Id') {
+                            colName = 'manager_id';
+                            value = parseInt(value);
+                        } else if (column === 'First name') {
+                            colName = 'first_name';
+                            value = `'${value}'`;
+                        } else if (column === 'Last name') {
+                            colName = 'last_name';
+                            value = `'${value}'`;
+                        };
+                        updateTable.employeeTable(colName, value, `id = ${employeeId}`, () => {
+                            console.log(`${column} was updated to ${value}.`);
+                        });
+                        setTimeout( () => {
+                            inquireAgain();
+                        }, 500);
+                    });
+                };
+            });
+        };
+    });
+};
+
 // --------------------------- INSERT------------------------ //
 
 const inquireInsert = () => {
     inquirer.prompt([
         {
             type: 'list',
-            message: `Which table you you like to add to?`,
+            message: `Select a table to add to:`,
             name: 'table',
             choices: [
                 'Departments',
